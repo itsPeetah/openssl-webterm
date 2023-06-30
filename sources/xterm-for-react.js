51df57b72b8bfc666f6f35efa1c0e51a02695ef0
itspeetah <pietro.g.moroni@gmail.com>
2023-06-30 12:57:51 +0200

    Stripped even more

--- sources/xterm-for-react.js
+++ sources/xterm-for-react.js
@@ -27,35 +27,12 @@ export default class Xterm extends React.Component {
     className: PropTypes.string,
     options: PropTypes.object,
     addons: PropTypes.array,
-    onBinary: PropTypes.func,
-    onCursorMove: PropTypes.func,
-    onData: PropTypes.func,
-    onKey: PropTypes.func,
-    onLineFeed: PropTypes.func,
-    onScroll: PropTypes.func,
-    onSelectionChange: PropTypes.func,
-    onRender: PropTypes.func,
-    onResize: PropTypes.func,
-    onTitleChange: PropTypes.func,
-    customKeyEventHandler: PropTypes.func,
   };
 
   constructor(props) {
     super(props);
 
     this.terminalRef = React.createRef();
-
-    // Bind Methods
-    this.onData = this.onData.bind(this);
-    this.onCursorMove = this.onCursorMove.bind(this);
-    this.onKey = this.onKey.bind(this);
-    this.onBinary = this.onBinary.bind(this);
-    this.onLineFeed = this.onLineFeed.bind(this);
-    this.onScroll = this.onScroll.bind(this);
-    this.onSelectionChange = this.onSelectionChange.bind(this);
-    this.onRender = this.onRender.bind(this);
-    this.onResize = this.onResize.bind(this);
-    this.onTitleChange = this.onTitleChange.bind(this);
 
     this.setupTerminal();
   }
@@ -69,25 +46,6 @@ export default class Xterm extends React.Component {
       this.props.addons.forEach((addon) => {
         this.terminal.loadAddon(addon);
       });
-    }
-
-    // Create Listeners
-    this.terminal.onBinary(this.onBinary);
-    this.terminal.onCursorMove(this.onCursorMove);
-    this.terminal.onData(this.onData);
-    this.terminal.onKey(this.onKey);
-    this.terminal.onLineFeed(this.onLineFeed);
-    this.terminal.onScroll(this.onScroll);
-    this.terminal.onSelectionChange(this.onSelectionChange);
-    this.terminal.onRender(this.onRender);
-    this.terminal.onResize(this.onResize);
-    this.terminal.onTitleChange(this.onTitleChange);
-
-    // Add Custom Key Event Handler
-    if (this.props.customKeyEventHandler) {
-      this.terminal.attachCustomKeyEventHandler(
-        this.props.customKeyEventHandler
-      );
     }
   }
 
@@ -103,46 +61,6 @@ export default class Xterm extends React.Component {
     // When the component unmounts dispose of the terminal and all of its listeners.
     this.terminal.dispose();
     // window.terminal = undefined;
-  }
-
-  onBinary(data) {
-    if (this.props.onBinary) this.props.onBinary(data);
-  }
-
-  onCursorMove() {
-    if (this.props.onCursorMove) this.props.onCursorMove();
-  }
-
-  onData(data) {
-    if (this.props.onData) this.props.onData(data);
-  }
-
-  onKey(event) {
-    if (this.props.onKey) this.props.onKey(event);
-  }
-
-  onLineFeed() {
-    if (this.props.onLineFeed) this.props.onLineFeed();
-  }
-
-  onScroll(newPosition) {
-    if (this.props.onScroll) this.props.onScroll(newPosition);
-  }
-
-  onSelectionChange() {
-    if (this.props.onSelectionChange) this.props.onSelectionChange();
-  }
-
-  onRender(event) {
-    if (this.props.onRender) this.props.onRender(event);
-  }
-
-  onResize(event) {
-    if (this.props.onResize) this.props.onResize(event);
-  }
-
-  onTitleChange(newTitle) {
-    if (this.props.onTitleChange) this.props.onTitleChange(newTitle);
   }
 
   render() {
